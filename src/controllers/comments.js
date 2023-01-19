9542d6050a82bc627984f08f02090285310e0665
Vladimir Dyblia <vladimirdublya@gmail.com>
2023-01-19 17:46:34 +0200

    added headers

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -3,12 +3,22 @@ import * as CommService from '../services/comments.js';
 export const getAll = async (req, res) => {
     const comments = await CommService.getAll();
 
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
+
     res.send(comments);
 };
 
 export const getOne = async (req, res) => {
     const { id } = req.params;
     const foundComm = await CommService.getById(id);
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
 
     if (!foundComm) { 
         res.sendStatus(404);
@@ -20,6 +30,11 @@ export const getOne = async (req, res) => {
 
 export const add = async (req, res) => {
     const { user_name, email, home_page, text, parent_id, file } = req.body;
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
 
     if (!text || !user_name || !email) {
         res.sendStatus(422);
