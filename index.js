b7919526fecf5b8a06fac4739f2589ebc3bcc378
Vladimir Dyblia <vladimirdublya@gmail.com>
2023-01-19 17:43:16 +0200

    added star

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = createServer(app);
 
 export const io = new Server(server,{
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
